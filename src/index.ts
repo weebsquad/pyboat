ddceb3772ae868631c5dec51f3e2792b1e674e22
MetalRuller <MetalRuller@users.noreply.github.com>
2020-08-06 14:11:47 +0100

    better

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-/// <reference path="E:/Dropbox/Dropbox/Stuff/Programas/Pylon/typings/index.d.ts" />
+/// <reference path="../typings/index.d.ts" />
 
 import {InitializeEvents} from './lib/eventHandler/handler';
 //import { InitializeCommands } from './lib/commands';
