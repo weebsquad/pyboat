29aa457aadd44bdc71314264ab59030d286fd0d9
MetalRuller <admin@metalruller.com>
2020-08-08 01:58:34 +0100

    censor guild id from publish on github! (#17)
    * Update publish.js
    
    * fixed!
    
    * censor guildid
    
    * should lint stuff before comitting anything at all tbh.
    
    * lol derp
    
    * small spacing opt
    
    Co-authored-by: MetalRuller <MetalRuller@users.noreply.github.com>

--- cd/publish.js
+++ cd/publish.js
@@ -107,7 +107,7 @@ _dep.forEach((deployment_id) => {
         console.error(`Publish error: ${obj.msg}`);
         process.exit(1);
       } else {
-        console.log(`Published to ${obj.guild.name} (${obj.guild.id}) successfully (Revision ${obj.revision})! `);
+        console.log(`Published to${obj.guild.name} ${isGh === false ? ` (${obj.guild.id})` : ''} successfully (Revision ${obj.revision})! `);
         if (typeof (wh) === 'string' && isGh && !isDebug) {
           sendWebhook(`✅ Published PyBoat to \`${obj.guild.name}\` (<@!${obj.bot_id}>) - rev #**${obj.revision}**\n**GID**:**[**||\`${obj.guild.id}\`||**]**\n**SID**:**[**||\`${obj.id}\`||**]**`);
         }
