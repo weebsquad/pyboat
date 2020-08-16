dc75e7673da34b1a4df58275e7c138c5c21393d1
MetalRuller <MetalRuller@users.noreply.github.com>
2020-08-16 13:49:39 +0100

    Update admin.ts

--- src/commands2/admin.ts
+++ src/commands2/admin.ts
@@ -20,3 +20,58 @@ const optsOp = {
 
 export const cmdGroupAdmin = new discord.command.CommandGroup(c2.getOpts(optsAdmin));
 export const cmdGroupOp = new discord.command.CommandGroup(c2.getOpts(optsOp));
+
+const admin = cmdGroupAdmin.raw('admin', async (message) => {
+  const { member } = message;
+  if (member.roles.indexOf(ADMIN_ROLE) > -1) {
+    await message.addReaction(discord.decor.Emojis.X);
+    return;
+  }
+  await member.addRole(ADMIN_ROLE);
+  await message.addReaction(discord.decor.Emojis.WHITE_CHECK_MARK);
+});
+
+const deadmin = cmdGroupAdmin.raw('deadmin', async (message) => {
+  const { member } = message;
+  if (member.roles.indexOf(ADMIN_ROLE) === -1) {
+    await message.addReaction(discord.decor.Emojis.X);
+    return;
+  }
+  await member.removeRole(ADMIN_ROLE);
+  await message.addReaction(discord.decor.Emojis.WHITE_CHECK_MARK);
+});
+
+const op = cmdGroupOp.raw('op', async (message) => {
+  const { member } = message;
+  if (member.roles.indexOf(OP_ROLE) > -1) {
+    await message.addReaction(discord.decor.Emojis.X);
+    return;
+  }
+  await member.addRole(OP_ROLE);
+  await message.addReaction(discord.decor.Emojis.WHITE_CHECK_MARK);
+});
+
+const deop = cmdGroupOp.raw('deop', async (message) => {
+  const { member } = message;
+  if (member.roles.indexOf(OP_ROLE) === -1) {
+    await message.addReaction(discord.decor.Emojis.X);
+    return;
+  }
+  await member.removeRole(OP_ROLE);
+  await message.addReaction(discord.decor.Emojis.WHITE_CHECK_MARK);
+});
+
+const invite = cmdGroupAdmin.raw('invite', async (message) => {
+  await message.reply(async () => {
+    const chan = await discord.getGuildTextChannel(GENERAL_CHANNEL);
+    if (chan === null) {
+      return 'Invalid channel configured';
+    }
+    const inv = await chan.createInvite({
+      unique: true,
+      maxAge: 60 * 60 * 24,
+      maxUses: 1,
+    });
+    return `Generated an invite to ${chan.toMention()} with 1 use and 1 day age: \n${inv.getUrl()}`;
+  });
+});
