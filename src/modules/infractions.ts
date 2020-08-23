df106465cc4a6c01d12cb6474f741a6ae557ed5e
MetalRuller <MetalRuller@users.noreply.github.com>
2020-08-23 18:01:13 +0100

    add help and docs cmds

--- src/modules/infractions.ts
+++ src/modules/infractions.ts
@@ -2,4 +2,18 @@ import * as utils from '../lib/utils';
 import * as c2 from '../lib/commands2';
 import { config, globalConfig, Ranks } from '../config';
 import { logCustom } from './logging/events/custom';
-import { getMemberTag } from './logging/utils';
+
+export function InitializeCommands() {
+  const F = discord.command.filters;
+
+  const _groupOptions = {
+    description: 'Infraction Commands',
+    filters: c2.getFilters('infractions', Ranks.Moderator),
+  };
+
+  const optsGroup = c2.getOpts(
+    _groupOptions,
+  );
+  const cmdGroup = new discord.command.CommandGroup(optsGroup);
+  return cmdGroup;
+}
