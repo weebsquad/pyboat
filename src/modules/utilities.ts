cea478ef13c437cc45841d7bc310ae1fb3419012
metal6 <metal@i0.tf>
2020-09-08 17:26:27 +0100

    fix server features formatting with _

--- src/modules/utilities.ts
+++ src/modules/utilities.ts
@@ -546,7 +546,7 @@ export function InitializeCommands() {
             discord.decor.Emojis.WHITE_CHECK_MARK
           } ( ${widgetChannel} )`
           : '';
-        const features = guild.features.length > 0 ? guild.features.map((feat) => feat.split(' ').map((sp) => `${sp.substr(0, 1).toUpperCase()}${sp.substr(1).toLowerCase()}`).join(' ')).join(', ') : 'None';
+        const features = guild.features.length > 0 ? guild.features.map((feat) => feat.split('_').map((sp) => `${sp.substr(0, 1).toUpperCase()}${sp.substr(1).toLowerCase()}`).join(' ')).join(', ') : 'None';
 
         desc += `  **❯ **Information
 <:rich_presence:735781410509684786>**ID**: \`${guild.id}\`
