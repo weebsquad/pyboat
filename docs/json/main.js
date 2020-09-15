b6bcef943a08891751e3710023f61ce396466b7c
metal6 <metal@i0.tf>
2020-09-15 15:25:25 +0100

    schema progress!

--- docs/json/main.js
+++ docs/json/main.js
@@ -54,7 +54,7 @@ fetch('./schema.json')
       schema: obj,
       theme: 'bootstrap4',
       disable_properties: true,
-      disable_edit_json: false,
+      disable_edit_json: true,
       disable_array_reorder: true,
       disable_array_delete_last_row: true,
       disable_array_delete_all_rows: true,
