3c6df2cb713d1e6e406b56388f88827e7a03b280
metal6 <metal@i0.tf>
2020-09-16 15:47:55 +0100

    add recursive sets fixing imports and direct link

--- docs/json/main.js
+++ docs/json/main.js
@@ -24,6 +24,10 @@ function parseData(dt, compose = true) {
   return dt;
 }
 
+const button_copy = document.querySelector('#button_copy');
+const button_download = document.querySelector('#button_download');
+const directLink = document.querySelector('#direct_link');
+const json_output = document.querySelector('#json_output');
 let editor;
 function submit() {
   editor.validate();
@@ -48,11 +52,19 @@ function download(filename, text) {
 }
 
 function getConfigJson() {
-  download('config.json', JSON.stringify(JSON.parse(document.getElementById('json_output').value), null, 2));
+  const vali = editor.validate();
+  if (vali.length) {
+    return;
+  }
+  download('config.json', JSON.stringify(JSON.parse(json_output.value), null, 2));
 }
 
 function copyJson() {
-  const copyText = document.getElementById('json_output');
+  const vali = editor.validate();
+  if (vali.length) {
+    return;
+  }
+  const copyText = json_output;
 
   /* Select the text field */
   copyText.select();
@@ -62,13 +74,65 @@ function copyJson() {
   document.execCommand('copy');
 }
 
+function recursiveSet(thisData, previousString) {
+  for (const key in thisData) {
+    const thisEditor = `${previousString}.${key}`;
+    if (typeof thisData[key] === 'object' && !Array.isArray(thisData[key]) && thisData[key] !== null) {
+      recursiveSet(thisData[key], thisEditor);
+      continue;
+    }
+    const edi = editor.getEditor(thisEditor);
+    if (edi) {
+      edi.setValue(thisData[key]);
+    }
+  }
+}
+
 function importJSON() {
-  const val = document.getElementById('import');
+  editor.disable();
   try {
-    const parsed = JSON.parse(val.value);
-    editor.setValue(parsed);
+    const val = JSON.parse(document.getElementById('import').value);
+    recursiveSet(val, 'root');
   } catch (e) {
     console.error(e);
+  }
+  editor.enable();
+}
+
+function updateDirectLink() {
+  const vali = editor.validate();
+  if (vali.length) {
+    return;
+  }
+  let url = window.location.href.replace(/\?.*/, '');
+  url += '?data=';
+  url += LZString.compressToBase64(JSON.stringify(JSON.parse(json_output.value)));
+  directLink.href = url;
+}
+
+function parseUrl() {
+  const url = window.location.search;
+  const queryParamsString = url.substring(1, url.length);
+  const queryParams = queryParamsString.split('&');
+
+  if (queryParamsString.length) {
+    queryParams.forEach((queryParam) => {
+      const splittedParam = queryParam.split('=');
+      const param = splittedParam[0];
+      const value = splittedParam[1];
+
+      // data query param
+      if (param === 'data') {
+        // compress schema and value
+        try {
+          data = JSON.parse(LZString.decompressFromBase64(value));
+          if (typeof data === 'object' && data !== null && typeof data.modules === 'object') {
+            recursiveSet(data, 'root');
+          }
+        } catch (reason) {
+        }
+      }
+    });
   }
 }
 
@@ -78,21 +142,25 @@ fetch('./schema.json')
     const defaultOptions = {
       iconlib: 'spectre',
       object_layout: 'table',
-      show_errors: 'always',
+      show_errors: 'interaction',
       schema: obj,
       theme: 'spectre',
       disable_properties: true,
       disable_edit_json: true,
       disable_array_reorder: true,
+      keep_oneof_values: true,
       disable_array_delete_last_row: true,
       disable_array_delete_all_rows: false,
       prompt_before_delete: false,
       required_by_default: false,
       show_opt_in: false,
+      no_additional_properties: false,
+      remove_empty_properties: true,
     };
 
     const element = document.getElementById('editor_holder');
     editor = new JSONEditor(element, defaultOptions);
+    editor.disable();
 
     /* document.getElementById('submit').addEventListener('click', () => {
   // Get the value from the editor
@@ -101,22 +169,37 @@ fetch('./schema.json')
     editor.on('change', () => {
       const vali = editor.validate();
       if (vali.length) {
-        document.getElementById('button_copy').style = 'background-color:red';
-        document.getElementById('button_copy').disabled = true;
-        document.getElementById('button_download').style = 'background-color:red';
-        document.getElementById('button_download').disabled = true;
-        document.getElementById('json_output').value = `JSON Error: ${JSON.stringify(vali, null, 2)}`;
+        button_copy.style = 'background-color:red';
+        button_copy.disabled = true;
+        button_download.style = 'background-color:red';
+        button_download.disabled = true;
+        json_output.value = `JSON Error: ${JSON.stringify(vali, null, 2)}`;
+        directLink.disabled = true;
+        directLink.style = 'background-color:red';
       } else {
-        document.getElementById('button_copy').style = 'background-color:green';
-        document.getElementById('button_copy').disabled = false;
-        document.getElementById('button_download').style = 'background-color:green';
-        document.getElementById('button_download').disabled = false;
-        document.getElementById('json_output').value = JSON.stringify(editor.getValue(), null, 2);
+        button_copy.style = 'background-color:green';
+        button_copy.disabled = false;
+        button_download.style = 'background-color:green';
+        button_download.disabled = false;
+        json_output.value = JSON.stringify(editor.getValue(), null, 2);
+        directLink.disabled = false;
+        directLink.style = 'background-color:green';
+        updateDirectLink();
       }
     });
     editor.on('ready', () => {
       // Now the api methods will be available
-      // editor.validate();
+      parseUrl();
+      editor.enable();
       console.log('ready');
+      editor.validate();
     });
   });
+
+button_copy.addEventListener('click', () => {
+  copyJson();
+});
+
+button_download.addEventListener('click', () => {
+  getConfigJson();
+});
